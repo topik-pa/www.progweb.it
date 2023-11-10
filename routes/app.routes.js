b74e7308f1791d10c6f34b234b8b91c33ba6e02f
Marco Pavan <marcopavan.mp@gmail.com>
2023-11-10 10:41:37 +0100

    Added new views

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -18,6 +18,53 @@ module.exports = app => {
     ]
     res.render('privacy/privacy', { id: 'privacy', title: 'Privacy', url: req.url, breadcrumbs })
   })
+
+  // TOPIC
+  app.get('/html', (req, res) => {
+    const breadcrumbs = [
+      {
+        name: 'HTML'
+      }
+    ]
+    res.render('topic/html/html', { id: 'html', title: 'HTML', url: req.url, breadcrumbs })
+  })
+  app.get('/javascript', (req, res) => {
+    const breadcrumbs = [
+      {
+        name: 'JavaScript'
+      }
+    ]
+    res.render('topic/js/js', { id: 'javascript', title: 'JavaScript', url: req.url, breadcrumbs })
+  })
+
+  // ARCHIVE
+  app.get('/settembre-2023', (req, res) => {
+    const breadcrumbs = [
+      {
+        name: 'Settembre 2023'
+      }
+    ]
+    res.render('archive/sept2023/sept2023', { id: 'sept2023', title: 'Settembre 2023', url: req.url, breadcrumbs })
+  })
+  app.get('/ottobre-2023', (req, res) => {
+    const breadcrumbs = [
+      {
+        name: 'Ottobre 2023'
+      }
+    ]
+    res.render('archive/oct2023/oct2023', { id: 'oct2023', title: 'Ottobre 2023', url: req.url, breadcrumbs })
+  })
+
+  // GRAPHS
+  app.get('/grafici', (req, res) => {
+    const breadcrumbs = [
+      {
+        name: 'Grafici'
+      }
+    ]
+    res.render('graphs/graphs', { id: 'graphs', title: 'Grafici', url: req.url, breadcrumbs })
+  })
+
   // POST
   app.get('/javascript-closure', (req, res) => {
     const breadcrumbs = [
