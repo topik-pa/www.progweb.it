1cc8bb012dfefe653acf88ae300acb835e87d778
Marco Pavan <marcopavan.mp@gmail.com>
2024-04-25 15:54:53 +0200

    try again

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(function (req, res, next) {
 })
 
 app.use(function (request, response, next) {
-  if (process.env.NODE_ENV === 'production' && !request.secure) {
+  if (process.env.TOHTTPS && !request.secure) {
     return response.redirect('https://' + request.headers.host + request.url)
   }
   next()
