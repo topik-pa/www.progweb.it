c3efcdf6a70ec2c7565415a88ff3e291dc1cfb7a
Marco Pavan <marcopavan.mp@gmail.com>
2024-08-24 10:24:19 +0200

    i18n configuration + label management + better images

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app.use('/views', express.static(path.join(__dirname, 'views')))
 const i18n = new I18n({
   locales: ['en', 'it'],
   defaultLocale: 'it',
+  objectNotation: true,
   directory: path.join(__dirname, 'locales')
 })
 app.use((req, res, next) => {
