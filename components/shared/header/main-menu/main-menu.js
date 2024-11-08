552b190ca16cf721fb15dbac947a2cd3049ad84d
Marco Pavan <marcopavan.mp@gmail.com>
2024-11-08 10:20:05 +0100

    !! main menu upgraded

--- components/shared/header/main-menu/main-menu.js
+++ components/shared/header/main-menu/main-menu.js
@@ -2,14 +2,35 @@ const mainMenu = {
   toggleMobileMenu: () => {
     const islandingpage = document.querySelector('body.landing')
     if (islandingpage) return
+    const $view = document.documentElement
     const $root = document.getElementById('main-menu')
     const $hamburger = document.querySelector('header .hamburger')
     const $closeMenu = $root.querySelector('.close')
-    $hamburger.addEventListener('click', () => {
-      $root.classList.toggle('show')
-    })
-    $closeMenu.addEventListener('click', () => {
-      $root.classList.remove('show')
+
+    $view.addEventListener('click', (e) => {
+      if (e.target === $hamburger) {
+        $root.classList.toggle('show')
+        return
+      }
+      if (e.target === $closeMenu) {
+        $root.classList.remove('show')
+        return
+      }
+
+      let parent = e.target.parentNode
+      let toBeClosed = true
+      while (parent) {
+        console.log(parent)
+        if (parent === $root) {
+          toBeClosed = false
+          break
+        } else {
+          parent = parent.parentNode
+        }
+      }
+      if (toBeClosed) {
+        $root.classList.remove('show')
+      }
     })
   }
 }
